e5975e815eee4a0d2c8c3b1b41f7512a9185afb6
ryancheung <ryancheung.go@gmail.com>
2025-07-14 16:01:15 +0800

    Update UWP TFM to net9.0

--- UWPGame1/Program.cs
+++ UWPGame1/Program.cs
@@ -11,7 +11,6 @@ namespace UWPGame1
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
         static void Main(string[] args)
         {
             realArgs = args;
